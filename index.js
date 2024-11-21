3c4cc946aae74a473ee245c6a2adbc300f39972a
Wavezy11 <wavezy.dd@gmail.com>
2024-11-21 10:47:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const token = 'YOUR_BOT_TOKEN';
+
+const token = process.env.BOT_TOKEN; 
 
 client.once('ready', () => {
     console.log('Bot is online!');
@@ -14,7 +16,7 @@ client.on('messageCreate', message => {
 
     if (command === 'ping') {
         message.channel.send('Pong!');
-    } 
+    }
 });
 
 client.login(token);
